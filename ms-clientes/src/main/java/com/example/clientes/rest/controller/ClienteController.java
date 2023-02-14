ab3f6472cb6cb35eec6e9ebb1e17fe6d044da3d1
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2023-02-14 18:07:10 -0300

    Melhorias

--- ms-clientes/src/main/java/com/example/clientes/rest/controller/ClienteController.java
+++ ms-clientes/src/main/java/com/example/clientes/rest/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.example.clientes.rest.controller;
 import com.example.clientes.model.entity.Cliente;
 import com.example.clientes.rest.dto.ClienteDTO;
 import com.example.clientes.service.impl.ClienteServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +12,16 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clientes")
+@Slf4j// Acesso aos log
 public class ClienteController {
 
     @Autowired
     private ClienteServiceImpl service;
 
     @GetMapping("/status")
+
     public String status() {
+        log.info("Obtendo status do Microserviço de clientes.");
         return "Microsserviços de Cliente Rodando!";
     }
 
